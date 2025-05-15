3633685500b90c1ab6706aefc3ab1a5900fbd68e
Juan_Diego <juan3312orrego@gmail.com>
2025-05-15 12:48:52 -0500

    Inicio de vistas causal
    Se agrega sbadmin2 como templates, se crea vista inicial para index y create de causal

---
+++ orderweb/resources/views/causal/create.blade.php
@@ -0,0 +1,26 @@
+@extends('templates.base')
+@section('title', 'Crear causal')
+@section('header', 'Crear causal')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 mb-4">
+            <form action="#" method="POST">
+                @csrf {{-- medida de seguridad de laravel para evitar ataques --}}
+                <div class="row form-group">
+                    <div class="col-lg-12 mb-4">
+                        <label for="description">Descripción</label>
+                        <input type="text" class="form-control" id="description" name="description" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 mb-4">
+                        <button type="submit" class="btn btn-primary btn-block">Guardar</button>
+                    </div>
+                    <div class="col-lg-6 mb-4">
+                        <a href="#" class="btn btn-secondary btn-block">Cancelar</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
--- orderweb/resources/views/templates/footer.blade.php
+++ orderweb/resources/views/templates/footer.blade.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html>    
     <body>
-        <footer class="copyright">
-            <span>Copyright &copy; SENA 2023</span>
+              
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright &copy; ORDER WEB 2024</span>
+                </div>
+            </div>
         </footer>
+        
     </body>
 </html>
