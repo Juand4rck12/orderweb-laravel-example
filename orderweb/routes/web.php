ee3536a46ba63a8a052cbb32371bbdb8067f64c0
Juan_Diego <juan3312orrego@gmail.com>
2025-06-05 09:37:29 -0500

    Registro P2 y Gates
    Se termina el registro y se configuran las Gates para el acceso a recursos por cada rol

--- orderweb/routes/web.php
+++ orderweb/routes/web.php
@@ -34,14 +34,14 @@ Route::prefix('auth')->group(function(){
     Route::get('/index', [AuthController::class, 'index']) -> name('auth.index');
     Route::post('/login', [AuthController::class, 'login']) -> name('auth.login');
     Route::get('/register', [AuthController::class, 'create']) -> name('auth.register');
-    Route::post('/register', [AuthController::class, 'store']) -> name('auth.register');
+    Route::post('/register', [AuthController::class, 'store']) -> name('auth.store');
 });
 
-Route::middleware('auth')->prefix('auth')->group(function(){
+Route::middleware(['auth', 'can:admin-supervisor'])->prefix('auth')->group(function(){
     Route::get('/logout', [AuthController::class, 'logout']) -> name('auth.logout');
 });
 
-Route::middleware('auth')->prefix('causal')->group(function(){
+Route::middleware(['auth', 'can:administrador'])->prefix('causal')->group(function(){
     Route::get('/index', [CausalController::class, 'index']) -> name('causal.index');
     Route::get('/create', [CausalController::class, 'create']) -> name('causal.create');
     Route::get('/edit/{id}', [CausalController::class, 'edit']) -> name('causal.edit');
@@ -50,7 +50,7 @@ Route::middleware('auth')->prefix('causal')->group(function(){
     Route::get('/destroy/{id}', [CausalController::class, 'destroy']) -> name('causal.destroy');
 });
 
-Route::middleware('auth')->prefix('type_activity')->group(function() {
+Route::middleware(['auth', 'can:administrador'])->prefix('type_activity')->group(function() {
     Route::get('/index', [TypeActivityController::class, 'index']) -> name('type_activity.index');
     Route::get('/create', [TypeActivityController::class, 'create']) -> name('type_activity.create');
     Route::get('/edit/{id}', [TypeActivityController::class, 'edit']) -> name('type_activity.edit');
@@ -60,7 +60,7 @@ Route::middleware('auth')->prefix('type_activity')->group(function() {
 });
 
 
-Route::middleware('auth')->prefix('observation')->group(function() {
+Route::middleware(['auth', 'can:administrador'])->prefix('observation')->group(function() {
     Route::get('/index', [ObservationController::class, 'index']) -> name('observation.index');
     Route::get('/create', [ObservationController::class, 'create']) -> name('observation.create');
     Route::get('/edit/{id}', [ObservationController::class, 'edit']) -> name('observation.edit');
@@ -70,7 +70,7 @@ Route::middleware('auth')->prefix('observation')->group(function() {
 });
 
 
-Route::middleware('auth')->prefix('technician')->group(function() {
+Route::middleware(['auth', 'can:supervisor'])->prefix('technician')->group(function() {
     Route::get('/index', [TechnicianController::class, 'index']) -> name('technician.index');
     Route::get('/create', [TechnicianController::class, 'create']) -> name('technician.create');
     Route::get('/edit/{id}', [TechnicianController::class, 'edit']) -> name('technician.edit');
@@ -80,7 +80,7 @@ Route::middleware('auth')->prefix('technician')->group(function() {
 });
 
 
-Route::middleware('auth')->prefix('activity')->group(function() {
+Route::middleware(['auth', 'can:admin-supervisor'])->prefix('activity')->group(function() {
     Route::get('/index', [ActivityController::class, 'index']) -> name('activity.index');
     Route::get('/create', [ActivityController::class, 'create']) -> name('activity.create');
     Route::get('/edit/{id}', [ActivityController::class, 'edit']) -> name('activity.edit');
@@ -90,7 +90,7 @@ Route::middleware('auth')->prefix('activity')->group(function() {
 });
 
 
-Route::middleware('auth')->prefix('order')->group(function() {
+Route::middleware(['auth', 'can:admin-supervisor'])->prefix('order')->group(function() {
     Route::get('/index', [OrderController::class, 'index']) -> name('order.index');
     Route::get('/create', [OrderController::class, 'create']) -> name('order.create');
     Route::get('/edit/{id}', [OrderController::class, 'edit']) -> name('order.edit');
