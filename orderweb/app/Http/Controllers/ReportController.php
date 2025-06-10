0da9a43c8ef5e9b9881d952853165abf81984649
Juan_Diego <juan3312orrego@gmail.com>
2025-06-10 09:44:55 -0500

    reportes P2
    Se termina reporte de técnicos y se crea reporte de actividades por técnico

--- orderweb/app/Http/Controllers/ReportController.php
+++ orderweb/app/Http/Controllers/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
 use App\Models\Technician;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
@@ -9,20 +10,42 @@ use Illuminate\Http\Request;
 class ReportController extends Controller
 {
     public function index () {
-        return view('reports.index');
+        $technicians = Technician::all();
+        return view('reports.index', compact('technicians'));
+    }
+
+    public function export_activities_by_technician(Request $request) {
+        $activities = Activity::where('technician_id', $request['technician_id'])->get();
+        
+        $data = array (
+            'activities' => $activities
+        );
+
+        $pdf = Pdf::loadView('reports.export_activities_by_technician', $data)
+            ->setPaper('letter', 'portrait')
+            ->setOptions([
+                'defaultFont' => 'sans-serif',
+                'isRemoteEnabled' => true
+            ]); // Landscape: horizontal
+
+        return $pdf->download('ActivitiesByTechnician-' . $request['technician_id'] . '.pdf');
     }
 
     /**
      * Reporte que genera el listado de todos los técnicos
      */
-    public function export_technician () {
+    public function export_technicians () {
         $technicians = Technician::all();
         $data = [
             'technicians' => $technicians
         ];
 
         $pdf = Pdf::loadView('reports.export_technicians', $data)
-            ->setTaper('letter', 'portrait'); // Landscape: horizontal
+            ->setPaper('letter', 'portrait')
+            ->setOptions([
+                'defaultFont' => 'sans-serif',
+                'isRemoteEnabled' => true
+            ]); // Landscape: horizontal
 
         return $pdf->download('technicians.pdf');
     }
